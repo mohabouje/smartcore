1db648109a2b9bff9cbfbf98c8623582620e61d1
mohabouje <mohabouje@gmail.com>
2018-12-08 23:30:31 +0100

    Implementing a DOA component to compute the direction of arrival.

---
+++ doa/include/doa.hpp
@@ -0,0 +1,76 @@
+#ifndef SMARTCORE_DOA_H
+#define SMARTCORE_DOA_H
+
+#include <memory>
+#include <vector>
+
+namespace score {
+
+    class DOA {
+    public:
+
+        /**
+         * @brief Build a block to compute the Direction of Arrival of an array of microphones.
+         * @param sample_rate Sampling rate in Hz.
+         * @param microphone_distances Distances between microphones.
+         * @param sound_speed Sound of the speed in m/sec.
+         */
+        explicit DOA(float sample_rate, float microphone_distances = 0.08127, float sound_speed = 343.2f);
+
+        /**
+         * @brief Default destructor
+         */
+        ~DOA();
+
+        /**
+         * @brief Returns the current sample rate in Hz.
+         * @return Sample rate in Hz.
+         */
+        float sampleRate() const;
+
+        /**
+         * @brief Sets the sample rate of the input microphones.
+         * @param sample_rate Sample rate in Hz.
+         */
+        void setSampleRate(float sample_rate);
+
+        /**
+         * @brief Returns the speed of the sound in m/secs
+         * @return Speed in m/sec
+         */
+        float soundSpeed() const;
+
+        /**
+         * @brief Sets the current speed in m/secs
+         * @param Speed in m/sec
+         */
+        void setSoundSpeed(float speed);
+
+        /**
+         * @brief Sets the group of microphones, given by pair of indexes
+         * @param microphone_groups Group of microphones
+         */
+        void setGroupMicrophones(const std::vector<std::pair<std::size_t, std::size_t>>& microphone_groups);
+
+        /**
+         * @brief Returns the group of microphones
+         * @return Group of microphones in pair of index.
+         */
+        const std::vector<std::pair<std::size_t, std::size_t>>& groupMicrophones() const;
+
+        /**
+         * @brief Computes the direction of arrival of the different microphones
+         *
+         * @param input Vector of arrays storing the input audio samples.
+         * @returns The direction of arrival.
+         */
+        float process(const std::vector<std::vector<float>>& microphone_inputs);
+
+    private:
+        struct Pimpl;
+        std::unique_ptr<Pimpl> pimpl_;
+    };
+
+}
+
+#endif //SMARTCORE_DOA_H
---
+++ doa/src/doa.cpp
@@ -0,0 +1,162 @@
+#include "doa.hpp"
+#include "../include/doa.hpp"
+
+#include <fftw3.h>
+
+#include <complex>
+#include <algorithm>
+#include <doa.hpp>
+
+
+using namespace score;
+struct DOA::Pimpl {
+
+    Pimpl(float sample_rate, float microphone_distances = 0.08127, float sound_speed = 343.2f) :
+            maximum_tau_(microphone_distances / sound_speed),
+            sample_rate_(sample_rate),
+            microphone_distances_(microphone_distances),
+            sound_speed_(sound_speed) {
+
+    }
+
+    ~Pimpl() {
+        reset();
+    }
+
+    void reset() {
+        if (signal_plan_ != nullptr) {
+            fftwf_destroy_plan(signal_plan_);
+            fftwf_destroy_plan(reference_plan_);
+            fftwf_destroy_plan(gcc_plan_);
+            fftwf_cleanup();
+        }
+    }
+
+    float gccPhat(const std::vector<float>& signal, const std::vector<float>& reference) {
+        const auto expected_size = 2 * signal.size();
+        if (expected_size != fft_size_) {
+            reset();
+
+            fft_size_ = (size_t) expected_size;
+            signal_.resize(expected_size);
+            signal_spectrum_.resize(expected_size);
+            reference_.resize(expected_size);
+            reference_.resize(expected_size);
+            gcc_.resize(expected_size);
+
+            signal_plan_ = fftwf_plan_dft_r2c_1d((int) expected_size, signal_.data(),
+                    reinterpret_cast<fftwf_complex*>(signal_spectrum_.data()), FFTW_ESTIMATE | FFTW_PRESERVE_INPUT);
+            reference_plan_ = fftwf_plan_dft_r2c_1d((int) expected_size, reference_.data(),
+                    reinterpret_cast<fftwf_complex*>(reference_spectrum_.data()), FFTW_ESTIMATE | FFTW_PRESERVE_INPUT);
+            gcc_plan_ = fftwf_plan_dft_c2r_1d((int) expected_size,
+                    reinterpret_cast<fftwf_complex*>(gcc_spectrum_.data()),
+                    gcc_.data(), FFTW_ESTIMATE | FFTW_PRESERVE_INPUT);
+        }
+
+        std::copy(signal.begin(), signal.end(), signal_.data());
+        std::fill(signal_.begin() + signal.size(), signal_.end(), 0.0f);
+
+        std::copy(reference.begin(), reference.end(), reference_.data());
+        std::fill(reference_.begin() + reference.size(), reference_.end(), 0.0f);
+
+        fftwf_execute(signal_plan_);
+        fftwf_execute(reference_plan_);
+
+        std::transform(signal_spectrum_.begin(), signal_spectrum_.end(),
+                reference_spectrum_.begin(), gcc_spectrum_.begin(), [](const auto& signal, const auto& reference) {
+            const auto operation = signal * std::conj(reference);
+            return operation / std::abs(operation);
+        });
+
+        fftwf_execute(gcc_plan_);
+
+        const auto maximum_tau_index = static_cast<std::size_t >(sample_rate_ * maximum_tau_);
+        const auto max_shift = std::min(expected_size / 2, maximum_tau_index);
+        const auto lambda = [](const auto left, const auto right) { return std::abs(left) < std::abs(right); };
+        const auto maximum_left = std::max_element(gcc_.begin(), gcc_.begin() + max_shift, lambda);
+        const auto maximum_right = std::max_element(gcc_.end() - max_shift, gcc_.end(), lambda);
+        const auto shift = std::distance(gcc_.begin(),
+                *maximum_left > *maximum_right ? maximum_left : maximum_right) - max_shift;
+
+        return shift / sample_rate_;
+    }
+
+
+    float process(const std::vector<std::vector<float>>& microphone_inputs) {
+
+        for (auto i = 0ul, size = tau_.size(); i < size; ++i) {
+            const auto group = microphone_groups_[i];
+            tau_[i] = gccPhat(microphone_inputs[group.first], microphone_inputs[group.second]);
+            theta_[i] = static_cast<int>(std::asin(tau_[i] / maximum_tau_) * 180.0f / M_PI);
+        }
+
+        const auto min_index = std::distance(tau_.begin(), std::min_element(tau_.begin(), tau_.end()));
+        auto best_guess = 0;
+        if ((min_index != 0 and theta_[min_index - 1] >= 0) or (min_index == 0 and theta_.back() < 0)) {
+            best_guess = (theta_[min_index] + 360) % 360;
+        } else {
+            best_guess = (180 - theta_[min_index]);
+        }
+
+        return (best_guess + 120 + min_index * 60) % 360;
+    }
+
+    void setGroupMicrophones(const std::vector<std::pair<std::size_t, std::size_t>>& microphone_groups) {
+        microphone_groups_ = microphone_groups;
+        tau_.resize(microphone_groups.size());
+        theta_.resize(microphone_groups.size());
+    }
+
+    std::vector<float> signal_{};
+    std::vector<float> reference_{};
+    std::vector<float> gcc_{};
+    std::vector<float> tau_;
+    std::vector<int> theta_;
+    std::vector<std::complex<float>> signal_spectrum_{};
+    std::vector<std::complex<float>> reference_spectrum_{};
+    std::vector<std::complex<float>> gcc_spectrum_{};
+    std::vector<std::pair<std::size_t, std::size_t>> microphone_groups_{};
+    std::size_t fft_size_{0};
+    float maximum_tau_{};
+    float sample_rate_{};
+    float microphone_distances_{};
+    float sound_speed_{};
+    fftwf_plan signal_plan_{nullptr};
+    fftwf_plan reference_plan_{nullptr};
+    fftwf_plan gcc_plan_{nullptr};
+};
+
+DOA::DOA(float sample_rate, float microphone_distances, float sound_speed) :
+    pimpl_(std::make_unique<Pimpl>(sample_rate, microphone_distances, sound_speed)){
+
+}
+
+void DOA::setGroupMicrophones(const std::vector<std::pair<std::size_t, std::size_t>>& microphone_groups) {
+    pimpl_->setGroupMicrophones(microphone_groups);
+}
+
+void DOA::setSoundSpeed(float sound_speed) {
+    pimpl_->sound_speed_ = sound_speed;
+}
+
+float DOA::soundSpeed() const {
+    return pimpl_->sound_speed_;
+}
+
+const std::vector<std::pair<std::size_t, std::size_t>> &score::DOA::groupMicrophones() const {
+    return pimpl_->microphone_groups_;
+}
+
+float DOA::process(const std::vector<std::vector<float>> &microphone_inputs) {
+    return pimpl_->process(microphone_inputs);
+}
+
+void DOA::setSampleRate(float sample_rate) {
+    pimpl_->sample_rate_ = sample_rate;
+}
+
+float DOA::sampleRate() const {
+    return pimpl_->sample_rate_;
+}
+
+DOA::~DOA() = default;
