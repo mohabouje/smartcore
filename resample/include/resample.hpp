1c8d085de5583863afd4d814aa0584c5c8bce5d6
Mohammed Boujemaoui Boulaghmoudi <mohabouje@gmail.com>
2018-12-08 19:43:13 +0100

    Using libsamplerate for basic resampling

--- resample/include/resample.hpp
+++ resample/include/resample.hpp
@@ -12,17 +12,27 @@ namespace score {
          * @brief The Quality enum describes the different Re-Sampler operation modes
          * in terms of output quality.
          */
-        enum Quality {
-            Low = 0,
-            High
+        enum class Quality {
+            BestQuality		= 0,
+            MediumQuality,
+            SincFastest,
+            ZeroOrder,
+            Linear
         };
 
         /**
          * @brief Creates a re sampler with the given configuration
          * @param quality  Quality of the re-sampling process.
-         * @param factor   Re-sampling factor (output sample rate / input sample rate)
+         * @param ratio   Re-sampling ratio (output sample rate / input sample rate)
          */
-        ReSampler(Quality quality, float factor);
+        ReSampler(std::size_t channels, Quality quality, float ratio);
+
+        /**
+         * Checks if the ratio is valid.
+         * @param ratio
+         * @return True if the ratio is valid, false otherwise.
+         */
+        static bool isValidRatio(float ratio);
 
         /**
          * @brief Default destructor
@@ -35,13 +45,33 @@ namespace score {
         void reset();
 
         /**
+         * @brief Returns the current ratio.
+         * @return Current ratio (output sample rate / input sample rate).
+         */
+        float ratio() const;
+
+
+        /**
+         * @brief Set a new ratio (output sample rate / input sample rate).
+         * @throws std::runtime_error If the ratio is invalid.
+         * @param ratio Output sample rate / input sample rate).
+         */
+        void setRatio(float ratio);
+
+        /**
+         * @brief Returns the current algorithm quality
+         * @return Quality of the algorithm.
+         */
+        Quality quality() const;
+
+        /**
          * @brief Performs a Re-Sampling filter in an audio frame.
          *
          * @param input Array storing the input audio samples.
          * @param output Array storing the output audio samples.
          * @returns The size of the output array.
          */
-        int process(const float* input, float* output, std::size_t size);
+        std::size_t process(const float* input, float* output, std::size_t size);
 
         /**
          * @brief Performs a Re-Sampling filter in an audio frame.
@@ -50,7 +80,7 @@ namespace score {
          * @param output Vector storing the output audio samples.
          * @returns The size of the output vector.
          */
-        int process(const std::vector<float>& input, std::vector<float>& output);
+        std::size_t process(const std::vector<float>& input, std::vector<float>& output);
 
     private:
         struct Pimpl;
--- resample/src/resample.cpp
+++ resample/src/resample.cpp
@@ -1,45 +1,77 @@
 #include "resample.hpp"
-#include <libresample.h>
+#include <samplerate.h>
+#include <resample.hpp>
+
 
 using namespace score;
 
 struct ReSampler::Pimpl {
 
-    Pimpl(Quality quality, float factor) :
-        quality_(quality),
-        factor_(factor),
-        handle_(resample_open(quality_, factor, factor)) {
+    Pimpl(int channels, Quality quality, float ratio) :
+            ratio_(ratio) {
 
-        if (factor <= 0.0) {
-            throw std::invalid_argument("Factor must be positive real numbers.");
+        if (!src_is_valid_ratio(ratio)) {
+            throw std::invalid_argument("ratio must be positive real numbers.");
         }
 
-        if (handle_ == nullptr) {
-            throw std::bad_alloc();
-        }
+        quality_ = static_cast<std::underlying_type<Quality >::type>(quality);
+        state_                   = src_new(quality_, channels, &error_);
+        ensure_no_error();
     }
 
     ~Pimpl() {
-        resample_close(handle_);
+        src_delete(state_);
     }
 
-    int process(const float* input, float* output, int size) {
-        int s_used = 0;
-        return resample_process(handle_, factor_, input, size, 0, &s_used, output, size);
+    long process(const float* input, float* output, int size) {
+        const auto channels = src_get_channels(state_);
+        data_.input_frames      = size / channels;
+        data_.output_frames     = size / channels;
+        data_.data_in           = input;
+        data_.data_out          = output;
+        data_.src_ratio         = ratio_;
+        data_.input_frames_used = 0;
+        data_.output_frames_gen = 0;
+        data_.end_of_input      = 0;
+        error_                  = src_process(state_, &data_);
+        ensure_no_error();
+        return data_.output_frames_gen * channels;
     }
 
     void reset() {
+        src_reset(state_);
+    }
 
+    float ratio() const {
+        return ratio_;
+    }
+
+    void setRatio(float ratio) {
+        ratio_ = ratio;
+        src_set_ratio(state_, ratio);
+        ensure_no_error();
+    }
+
+    void ensure_no_error() {
+        if (error_ != 0) {
+            throw std::runtime_error("Error while running re-sampling:" + std::string(src_strerror(error_)));
+        }
+    }
+
+    int quality() const {
+        return quality_;
     }
 
 private:
-    Quality quality_{Quality::Low};
-    float factor_{0.0};
-    void* handle_{nullptr};
+    SRC_DATA data_{};
+    SRC_STATE* state_{nullptr};
+    int error_{0};
+    int quality_{SRC_SINC_MEDIUM_QUALITY};
+    float ratio_{1};
 };
 
-ReSampler::ReSampler(Quality quality, float factor) :
-    pimpl_(std::make_unique<Pimpl>(quality, factor)) {
+ReSampler::ReSampler(std::size_t channels, Quality quality, float ratio) :
+    pimpl_(std::make_unique<Pimpl>(channels, quality, ratio)) {
 
 }
 
@@ -49,13 +81,29 @@ void ReSampler::reset() {
     pimpl_->reset();
 }
 
-int ReSampler::process(const float* input, float* output, std::size_t size) {
-    return pimpl_->process(input, output, static_cast<int>(size));
+std::size_t ReSampler::process(const float* input, float* output, std::size_t size) {
+    return static_cast<size_t>(pimpl_->process(input, output, static_cast<int>(size)));
 }
 
-int ReSampler::process(const std::vector<float>& input, std::vector<float>& output) {
+std::size_t ReSampler::process(const std::vector<float>& input, std::vector<float>& output) {
     output.resize(input.size());
     const auto used = pimpl_->process(input.data(), output.data(), static_cast<int>(input.size()));
     output.resize((unsigned long) used);
-    return used;
+    return (size_t) used;
+}
+
+float ReSampler::ratio() const {
+    return pimpl_->ratio();
+}
+
+void score::ReSampler::setRatio(float ratio) {
+    pimpl_->setRatio(ratio);
+}
+
+bool score::ReSampler::isValidRatio(float ratio) {
+    return static_cast<bool>(src_is_valid_ratio(ratio));
+}
+
+ReSampler::Quality score::ReSampler::quality() const {
+    return static_cast<Quality>(pimpl_->quality());
 }
